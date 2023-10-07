5f4d9c6058e3e002a755e78503418eb46345a0f7
M. Sean Davis Jr <35494197+Rainmaker0102@users.noreply.github.com>
2023-10-07 16:32:10 -0400

    Tiles, logo, and basic navigation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,7 @@
-extends Area2D
+extends CharacterBody2D
 
+@export var speed = 400
+var look = "left"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +10,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	get_input()
+	move_and_slide()
+
+func get_input():
+	walking_check()
+	var input_direction = Input.get_vector("move_left", "move_right", "null", "null")
+	velocity = input_direction * speed
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://TitleScreen.tscn")
+
+func walking_check():
+	if Input.is_action_pressed("move_left"):
+		if look == "right":
+			$AnimatedSprite2D.set_flip_h(true)
+			look = "left"
+		$AnimatedSprite2D.play("walking")
+	if Input.is_action_pressed("move_right"):
+		if look == "left":
+			$AnimatedSprite2D.set_flip_h(false)
+			look = "right"
+		$AnimatedSprite2D.play("walking")
+	if Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right"):
+		$AnimatedSprite2D.play("default")
