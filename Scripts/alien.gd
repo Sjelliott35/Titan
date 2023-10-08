91c21d448d06aa973d50696f71a83345b965c8ec
M. Sean Davis Jr <35494197+Rainmaker0102@users.noreply.github.com>
2023-10-08 10:14:35 -0400

    Desert level finished, start on tundra
    Also refactored some of the npcs

---
+++ Scripts/alien.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play("default")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
