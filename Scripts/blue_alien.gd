72797659c74210ce54c0ba3a09bdb40b278e0e3a
M. Sean Davis Jr <35494197+Rainmaker0102@users.noreply.github.com>
2023-10-08 13:48:22 -0400

    last level finished

--- Scripts/blue_alien.gd
+++ Scripts/blue_alien.gd
@@ -1,16 +1,11 @@
-extends Node2D
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Player.velocity.y = 400
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_alien_body_entered(body):
-	if body.get_name() == "Player":
-		get_tree().change_scene_to_file("res://ice_cave_level.tscn")
